f18c1d362d64fa5fd575bb64adef45227a83dfb6
Eswar Sai <eswars@infinite.com>
2024-06-26 08:04:49 -0700

    cc1

---
+++ assesments/cc1/cc1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace cc1
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            int i = 0, n = 5, product;
+            do
+            {
+                product = (n * i);
+                Console.WriteLine("{0} * {1} = {2}", n, i, product);
+                i++;
+            }
+            while (i <= 10);
+            Console.Read();
+
+        }
+    }
+}
---
+++ assesments/cc1/cc1/program2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cc1
+{
+    public class Program2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exchange First and Last character in a given string");
+            Console.WriteLine("---------------------------------------------------");
+            Console.WriteLine(First_Last("Eswarsai"));
+            Console.WriteLine(First_Last("V"));
+            Console.WriteLine(First_Last("iswarsaE"));
+            Console.Read();
+        }
+        public static string First_Last(string ustr)
+        {
+            return ustr.Length > 1
+               ? ustr.Substring(ustr.Length - 1) + ustr.Substring(1, ustr.Length - 2) + ustr.Substring(0, 1)
+               : ustr;
+           
+        }
+    }
+}
