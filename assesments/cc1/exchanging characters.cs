37573d59a539cf4cdf1c977da912e50939e93523
Eswar Sai <eswars@infinite.com>
2024-06-26 04:30:18 -0700

    cc2

---
+++ assesments/cc1/exchanging characters.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace exchanging_characters
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exchange First and Last character in a given string");
+            Console.WriteLine("---------------------------------------------------");
+            Console.WriteLine(First_Last("Eswarsai"));
+            Console.WriteLine(First_Last("V"));
+            Console.WriteLine(First_Last("iswarsaE"));
+        }
+        public static string First_Last(string ustr)
+        {
+            return ustr.Length > 1
+               ? ustr.Substring(ustr.Length - 1) + ustr.Substring(1, ustr.Length - 2) + ustr.Substring(0, 1)
+               : ustr;
+        }
+    }
+}
