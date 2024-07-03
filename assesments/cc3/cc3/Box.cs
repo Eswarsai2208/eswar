215a49b61558c577b7194e4e955c889d3a4a852e
Eswar Sai <eswars@infinite.com>
2024-07-03 00:35:48 -0700

    cc3

---
+++ assesments/cc3/cc3/Box.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cc3
+{
+    public class Box
+    {
+        public double Length { get; set; }
+        public double Breath { get; set; }
+        public Box(double lenght, double breath)
+        {
+            Length = lenght;
+            Breath = breath;
+        }
+        public static Box Add(Box box1, Box box2)
+        {
+            double combinedLength = box1.Length + box2.Length;
+            double combinedBreath = box1.Breath + box2.Breath;
+            return new Box(combinedLength, combinedBreath);
+        }
+        public void Display()
+        {
+            Console.WriteLine($"Box Dimension - Length: {Length}, Breath: {Breath}");
+        }
+        public class box
+        {
+            public static void Main()
+            {
+                Box box1 = new Box(2.0, 2.0);
+                Box box2 = new Box(4.0, 8.0);
+                Box box3 = Box.Add(box1, box2);
+                Console.WriteLine("Dimension of Box 1:");
+                box1.Display();
+                Console.WriteLine("Dimension of Box 2:");
+                box2.Display();
+                Console.WriteLine("Dimension of Box 3:");
+                box3.Display();
+                Console.Read();
+            }
+        }
+    }
+}
---
+++ assesments/cc3/cc3/IPL.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cc3
+{
+    public class Cricket
+    {
+        private double[] scores;
+        private int numMatches;
+
+        public void Initialize()
+        {
+            Console.Write("Enter the number of matches: ");
+            numMatches = Convert.ToInt32(Console.ReadLine());
+            scores = new double[numMatches];
+        }
+        public void InputScores()
+        {
+            for (int i = 0; i < numMatches; i++)
+            {
+                Console.Write($"Enter score for match {i + 1}: ");
+                scores[i] = Convert.ToDouble(Console.ReadLine());
+            }
+        }
+        public void CalculateAndDisplayResults()
+        {
+            double totalSum = 0;
+            foreach (double score in scores)
+            {
+                totalSum += score;
+            }
+            double average = totalSum / numMatches;
+            Console.WriteLine($"Total sum of scores: {totalSum}");
+            Console.WriteLine($"Average score: {average}");
+            Console.Read();
+        }
+    }
+    class IPL
+    {
+        static void Main()
+        {
+            Cricket cricket = new Cricket();
+            cricket.Initialize();
+            cricket.InputScores();
+            cricket.CalculateAndDisplayResults();
+        }
+    }
+}
